02dd1e4ac159f0c79b18ce9684de1d45e15ea486
Yasha Mouradi <yasha@onprem.com>
2020-02-20 19:50:17 +0000

    payments

--- models/orders.sql
+++ models/orders.sql
@@ -5,11 +5,10 @@ payments as (
     select * from {{ ref('stg_payments') }}
 )
 select orders.customer_id,orders.order_date, orders.order_id,
-    sum(payments.order_amount) as order_total_payments, 
+    sum(payments.payment_amount) as order_total_payments, 
     min(payments.payment_date) as first_payment_date, 
     max(payments.payment_date) as last_payment_date,
     count(payments.payment_id) as payment_count
 from orders
-left join payments
-on orders.order_id = payments.order_id
-group by orders.order_id
+left join payments using(order_id)
+group by orders.customer_id,orders.order_date, orders.order_id
